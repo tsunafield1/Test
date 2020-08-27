c308e294a87d86f06697271bcb6698d527ef8ad1
GLIT RUNGROJKIJKUL <63010022@KMITL.AC.TH>
2020-08-27 23:29:25 +0700

    test1

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,9 +1,26 @@
-#include<stdio.h>
-
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 int main()
 {
-	printf("Test");
-	printf("\n2ndTest");
-	printf("\n3rdTest");
-	return 0;
+    int n,sum;
+    scanf("%d", &n);
+    printf("%d ",n);
+    while (n > 10)
+    {
+        sum = 0;
+        int m = 10;
+        while (n / m > 0)
+        {
+            m *= 10;
+        }
+        m /= 10;
+        while (m >= 1)
+        {
+            sum += n / m;
+            n %= m;
+            m /= 10;
+        }
+        n = sum;
+        printf("-> %d", sum);
+    }
 }
